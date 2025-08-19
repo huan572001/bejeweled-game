1ff6d84d744d44febf00c70488368cfba1063fbd
huanlainetalabs <huanlai@netalabs.com>
2025-08-19 09:56:06 +0700

    update score

--- src/lib/game/engine/GameConfig.ts
+++ src/lib/game/engine/GameConfig.ts
@@ -1,5 +1,5 @@
 export const GRID_SIZE = 8;
 export const TILE_SIZE = 65;
 export const GEM_TYPES = 7;
-export const INITIAL_SCORE_GOAL = 5000;
+export const INITIAL_SCORE_GOAL = 1000;
 export const INITIAL_TIMER_MINUTES = 3;
